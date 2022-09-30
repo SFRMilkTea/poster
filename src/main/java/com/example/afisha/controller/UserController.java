4fda12bef8fad8a6b643935b78649ce61010f32b
SFRMilkTea <bayliz@bk.ru>
2022-09-30 13:18:01 +0700

    реализовано добавление пользователя в бд

--- src/main/java/com/example/afisha/controller/UserController.java
+++ src/main/java/com/example/afisha/controller/UserController.java
@@ -1,13 +1,29 @@
 package com.example.afisha.controller;
 
+import com.example.afisha.entity.UserEntity;
+import com.example.afisha.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity registration (@RequestBody UserEntity user){
+        try{
+            userRepository.save(user);
+            return ResponseEntity.ok("Пользователь " + user.getUsername() + " добавлен (точно добавлен)");
+        }
+        catch (Exception e) {
+            return ResponseEntity.badRequest().body("Произошла ошибка");
+        }
+    }
+
 
     @GetMapping
     public ResponseEntity getUsers(){
---
+++ src/main/java/com/example/afisha/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.afisha.repository;
+
+import com.example.afisha.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+}
