5058bfa7d919faccb19c2e8d4afadcb76d039136
SFRMilkTea <bayliz@bk.ru>
2022-10-03 10:53:48 +0700

    добавлена ошибка "Пользователь не найден"

---
+++ src/main/java/com/example/afisha/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package com.example.afisha.exception;
+
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
+
